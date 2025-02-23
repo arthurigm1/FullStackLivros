082ea7d5d4ab4b622afeafd6b52666bbd9696721
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-02-22 23:13:47 -0300

    cors update

--- src/main/java/projetolivros/livros/Controller/cors/CorsConfig.java
+++ src/main/java/projetolivros/livros/Controller/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://pagiinova.netlify.app")
-                        .allowedOrigins("http://localhost:4200")
+
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/projetolivros/livros/Security/SecurityConfig.java
+++ src/main/java/projetolivros/livros/Security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://pagiinova.netlify.app"));
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
