07d306abd7617e72bbd82c62d87a7a37a65a38c7
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-02-24 18:40:47 -0300

    arrumando emailsender

--- src/main/java/projetolivros/livros/Service/MailService.java
+++ src/main/java/projetolivros/livros/Service/MailService.java
@@ -22,7 +22,7 @@ public class MailService {
         String toAddres = user.getEmail();
         String fromAddres = "Pagiinova@gmail.com";
         String senderName = "`Pagiinova`";
-        String subject = "Por favor confirme seu cadastro";
+        String subject = "Por favor confirme seu cadastro!";
 
         String content = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional //EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">\n" +
